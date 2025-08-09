0d33ee7b8d85f043513ca148fd2832f08d48e851
testcodee <mp3cafe@naver.com>
2025-08-09 23:45:00 +0900

    충돌 해결

---
+++ forthefuture/build/resources/main/static/js/user/login.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.querySelector('.login-form');
+
+    loginForm.addEventListener('submit', function(event) {
+        // 폼 제출 전에 클라이언트 측 유효성 검사를 추가할 수 있습니다
+    });
+});
