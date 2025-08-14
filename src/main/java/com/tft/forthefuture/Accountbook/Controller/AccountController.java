49073053aff355881cee7b2c460d747b3f87e028
testcodee <mp3cafe@naver.com>
2025-08-14 17:46:03 +0900

    자산정보 화면

--- src/main/java/com/tft/forthefuture/Accountbook/Controller/AccountController.java
+++ src/main/java/com/tft/forthefuture/Accountbook/Controller/AccountController.java
@@ -2,7 +2,11 @@ package com.tft.forthefuture.Accountbook.Controller;
 
 import com.tft.forthefuture.Accountbook.Service.AccountService;
 import com.tft.forthefuture.Accountbook.Service.DashboardService;
+import com.tft.forthefuture.Accountbook.Vo.Account;
+import com.tft.forthefuture.User.Vo.User;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +23,23 @@ public class AccountController {
     }
 
     @GetMapping("/assets")
-    public String assets(Model model) {
+    public String assets(Model model, HttpSession session) {
+        User user = (User) session.getAttribute("loggedInUser");
+        if (user == null) {
+            return "redirect:/login";
+        }
+        /*TODO List
+        * 1. 총 자산
+        * 2. 총 부채
+        * 3. 순 자산
+        * 4. 계좌 별 잔액 관리
+        *  - accounts 테이블
+        *   balance : 현재 잔액
+        *   account_name : 계좌명 (예: 현금, 우리은행 통장, 국민카드)
+        *   account_type : 계좌 유형 (예: 현금, 은행, 신용카드, 투자)
+        * */
+        Account account = accountService.getAccountByUserId(user.getId());
+
         model.addAttribute("totalAssets","100000");
         model.addAttribute("totalDebt","100000");
         model.addAttribute("netWorth","-100000");
--- src/main/java/com/tft/forthefuture/Accountbook/Service/AccountService.java
+++ src/main/java/com/tft/forthefuture/Accountbook/Service/AccountService.java
@@ -2,6 +2,7 @@ package com.tft.forthefuture.Accountbook.Service;
 
 import com.tft.forthefuture.Accountbook.Mapper.AccountMapper;
 import com.tft.forthefuture.Accountbook.Mapper.TransactionMapper;
+import com.tft.forthefuture.Accountbook.Vo.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,10 @@ public class AccountService {
     public AccountService(AccountMapper accountMapper, TransactionMapper transactionMapper) {
         this.accountMapper = accountMapper;
         this.transactionMapper = transactionMapper;
+    }
+
+    public Account getAccountByUserId(Long userId) {
+       return accountMapper.findById(userId);
     }
 
 
