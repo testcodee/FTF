2a5230d6f2c1571855117c9e9ac950d9b374b88c
testcodee <mp3cafe@naver.com>
2025-09-17 16:47:02 +0900

    하단네비게이션 수정,

--- src/main/resources/static/js/components/bottom-nav.js
+++ src/main/resources/static/js/components/bottom-nav.js
@@ -19,20 +19,4 @@ $(document).ready(function() {
         }
         console.log("location pathname = " + window.location.pathname);
     }, 200);
-
-});
-
-    document.addEventListener('click', function(e) {
-    // 클릭된 요소가 .nav-item 또는 그 하위 요소인지 확인
-    if(e.target.closest('.nav-item')) {
-        const navItem = e.target.closest('.nav-item');
-
-        // 모든 메뉴에서 active 클래스 제거
-        document.querySelectorAll('.nav-item').forEach(item => {
-            item.classList.remove('active');
-        });
-
-        // 클릭한 메뉴에만 active 클래스 추가
-        navItem.classList.add('active');
-    }
 });
